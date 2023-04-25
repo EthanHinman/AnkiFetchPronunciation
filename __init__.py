b0ff3b69052127bc46961873fc4157c9fcc67c86
EthanHinman <hinmanet@umich.edu>
2023-04-25 15:40:02 -0400

    Drop down menu added to widget

--- __init__.py
+++ __init__.py
@@ -5,16 +5,23 @@ from aqt.utils import showInfo, qconnect
 # import all of the Qt GUI library
 from aqt.qt import *
 
+def getAllUserDecks():
+    pass
 
 class FetchPronunciationWindow(QMainWindow):
      def __init__(self):
         super().__init__()
 
         self.setWindowTitle("fetchPronunciation")
-        e1 = QLineEdit("Enter language of deck.")
-        e1.setMaxLength(20)
-        e1.setAlignment(Qt.AlignLeft)
-        e1.setFont(QFont("Arial",14))
+        userDecksList = getAllUserDecks()
+        userDecksComboBox = QComboBox()
+        userDecksComboBox.addItem("rekt")
+        # for deck in userDecksList:
+        #     userDecksComboBox.addItem(deck)
+        # e1 = QLineEdit("Enter language of deck.")
+        # e1.setMaxLength(20)
+        # e1.setAlignment(Qt.AlignLeft)
+        # e1.setFont(QFont("Arial",14))
         #e1.editingFinished.connect(self.enterPress)
         
         # deckName, done2 = QInputDialog.getText(
@@ -23,11 +30,20 @@ class FetchPronunciationWindow(QMainWindow):
         continueButton = QPushButton("Continue.")
         # redoButton = QPushButton("Re-enter.")
         # Set the central widget of the Window. Need to learn to put this in bottom right
-        flo = QFormLayout()
-        flo.addRow("Language of Deck: ", e1)
+        # flo = QFormLayout()
+        # flo.addRow("Language of Deck: ", e1)
         # flo.addButton(continueButton)
         #self.setLayout(flo)
         # self.setCentralWidget(continueButton)
+
+        layout = QVBoxLayout()
+        layout.addWidget(userDecksComboBox)
+
+
+        container = QWidget()
+        container.setLayout(layout)
+        self.setCentralWidget(container)
+        
 
 def fetchPronunciation():
     mw.myWidget = widget = FetchPronunciationWindow()
@@ -40,11 +56,11 @@ def fetchPronunciation():
 
 
 
-# create a new menu item, "fetchPronouciationForSet"
-action = QAction("fetchPronouciationForSet", mw)
+# create a new menu item, "fetchPronunciationForSet"
+action = QAction("fetchPronunciationForSet", mw)
 
 # set it to call fetchPronouciationForSet when it's clicked
-qconnect(action.triggered, fetchPronouciation)
+qconnect(action.triggered, fetchPronunciation)
 
 # and add it to the tools menu
 mw.form.menuTools.addAction(action)
