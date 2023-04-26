09cad0910c72b4ea0c3b96701d0f7fba2acffc62
Ethan <hinmanet@umich.edu>
2023-04-26 10:01:46 -0400

    widget-work in progress.

--- __init__.py
+++ __init__.py
@@ -5,29 +5,35 @@ from aqt.utils import showInfo, qconnect
 # import all of the Qt GUI library
 from aqt.qt import *
 
-def getAllUserDecks():
-    pass
+from anki import decks
 
 class FetchPronunciationWindow(QMainWindow):
      def __init__(self):
         super().__init__()
 
         self.setWindowTitle("fetchPronunciation")
-        userDecksList = getAllUserDecks()
+        
+        # Fetch all decks by name and add them to the dropdown bar for the user to select.
+        allDecksByName = mw.col.decks.all_names() # maybe I should get by id as well. What happens if the user has decks of the same name? Users fault.
         userDecksComboBox = QComboBox()
-        userDecksComboBox.addItem("rekt")
-        # for deck in userDecksList:
-        #     userDecksComboBox.addItem(deck)
-        # e1 = QLineEdit("Enter language of deck.")
-        # e1.setMaxLength(20)
-        # e1.setAlignment(Qt.AlignLeft)
-        # e1.setFont(QFont("Arial",14))
+        userDecksComboBox.setPlaceholderText("Select deck from options below.")
+        for deck in allDecksByName:
+         userDecksComboBox.addItem(str(deck))
+
+        whichCardSide = QComboBox()
+        whichCardSide.setPlaceholderText("Select Front or Back.")
+        whichCardSide.addItem("Front")
+        whichCardSide.addItem("Back")
+        
+        e1 = QLineEdit("Enter language of deck as 2 letters. See forvo api demo. https://api.forvo.com/demo")
+        e1.setMaxLength(100)
+        #e1.setAlignment(Qt.AlignLeft)
         #e1.editingFinished.connect(self.enterPress)
         
         # deckName, done2 = QInputDialog.getText(
         #     self, 'Input Dialog', 'Enter your desired deck name: ')
 
-        continueButton = QPushButton("Continue.")
+
         # redoButton = QPushButton("Re-enter.")
         # Set the central widget of the Window. Need to learn to put this in bottom right
         # flo = QFormLayout()
@@ -38,12 +44,35 @@ class FetchPronunciationWindow(QMainWindow):
 
         layout = QVBoxLayout()
         layout.addWidget(userDecksComboBox)
-
+        layout.addWidget(whichCardSide)
+        layout.addWidget(e1)        
+        continueButton = QPushButton("Continue.")
+        
+        layout.addWidget(continueButton)
 
         container = QWidget()
         container.setLayout(layout)
         self.setCentralWidget(container)
+        continueButton.clicked.connect(checkAllFieldsEntered(layout))
         
+def checkAllFieldsEntered(layout: QVBoxLayout):
+   if True:
+     showInfo(str(layout[QComboBox]))
+   elif layout[QComboBox][0]:
+    showInfo("button is working.")
+   elif layout[QComboBox][1]:
+    pass
+   elif True:
+    pass
+   elif True:
+    pass
+   elif True:
+    pass
+   else:
+    # everything is entered correctly. close the widget and get all pronunciations/
+    showinfo("Please select a deck.")
+    return
+   
 
 def fetchPronunciation():
     mw.myWidget = widget = FetchPronunciationWindow()
