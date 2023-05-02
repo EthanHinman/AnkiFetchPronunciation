4e55b6c00576f6d95015f330d228b813692a9595
Ethan <hinmanet@umich.edu>
2023-05-02 10:40:54 -0400

    Test to see if gitIgnoreWorks

--- forvoAPIKey.py
+++
@@ -1,5 +0,0 @@
-#############################################
-# This is your forvo API key. Please change NULL to your API key value.
-# You can get one by going to https://api.forvo.com
-forvoAPIKey = NULL
-#############################################
