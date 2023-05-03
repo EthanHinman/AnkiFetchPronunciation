45f51fd2e5dc27603784f3abff0dfe1d5827ebae
Ethan <hinmanet@umich.edu>
2023-05-03 15:43:01 -0400

    can do remote link, need to figure out how to download the file

--- __init__.py
+++ __init__.py
@@ -7,11 +7,15 @@ from aqt.qt import *
 
 from anki import decks
 
-from forvoAPIKey import forvoAPIKeyVariable
+from pathlib import Path
 
+##############################################################
+from forvoAPIKey import forvoAPIKeyVariable # DELETE THIS LINE. This is to avoid pushing my API key to the git.
+forvoAPIKeyVariable = forvoAPIKeyVariable # CHANGE THIS LINE TO forvoAPIKeyVariable = yourTokenHere
+##############################################################
 from anki.collection import Collection
 
-import os
+import requests
 #############################################
 # This is your forvo API key. 
 class FetchPronunciationWindow(QMainWindow):
@@ -78,7 +82,7 @@ def checkAllFieldsEntered(userDecksComboBox: QComboBox, whichCardSide :QComboBox
     deckManagerObject = decks.DeckManager(mw.col)
     listCardId = deckManagerObject.cids(mw.col.decks.by_name(userDecksComboBox.currentText())['id'])
     for cardId in listCardId:
-       fetchPronunciationForCard(cardId, languageInputWidget.text().lower(), whichCardSide.currentText() )
+       fetchPronunciationForCard(cardId, languageInputWidget.text().lower(), whichCardSide.currentText())
     return
 
 # INPUT: A card from a deck.
@@ -98,9 +102,41 @@ def fetchPronunciationForCard(cardId: int, language: str, cardSide: str)-> bool:
   
   # contact the forvo api to see if a pronouciation exists.
   # https://api.forvo.com/documentation/standard-pronunciation/
+  # https://www.w3schools.com/python/ref_dictionary_get.asp1
+    #################################################################
   formattedRequestString = "https://apifree.forvo.com/key/" + forvoAPIKeyVariable + "/format/json/action/standard-pronunciation/word/" + word + "/language/" + language
+  #################################################################
   # showInfo(formattedRequestString)
-  # audioLink = requests.get(formattedRequestString)
+  # formattedRequestString = "temp"
+  #################################################################
+  audioLinkJson = True#requests.get(formattedRequestString)
+  #################################################################
+  if audioLinkJson:
+    # showInfo(str(audioLinkJson.json()))
+    #audioLink = audioLinkJson.json()["items"][0]["pathmp3"]
+    audioMP3 = "https://apifree.forvo.com/audio/2p2m1l2q212h35242d1f3e3p2d2i2j3j1l343f3q1b2d2o3q2m222m2a32283l3k392l22293h31273f353h2q3c1n2e2f223k1j1f3e3q273f323i3c3n1k39272i1k2g1j261f1f3o3l3l271b2k3j3h3l363l2a3h1j3k2i2h1t1t_1m3o2d2d3p2j381h3f3e3d1f2f3e3d343f2b3k1m3k211t1t"#requests.get(audioLink) # now get the audio file
+    # open a file in audioFiles
+    filePath = Path("/home/ankiMP3/" + word + ".mp3")
+    filePath.touch(exist_ok=True, parents=True)
+    file= open(filePath, 'w')
+    # sound:https://apifree.forvo.com/audio/2p2m1l2q212h35242d1f3e3p2d2i2j3j1l343f3q1b2d2o3q2m222m2a32283l3k392l22293h31273f353h2q3c1n2e2f223k1j1f3e3q273f323i3c3n1k39272i1k2g1j261f1f3o3l3l271b2k3j3h3l363l2a3h1j3k2i2h1t1t_1m3o2d2d3p2j381h3f3e3d1f2f3e3d343f2b3k1m3k211t1t
+    # write audioMP3 to it if it exists
+    file.write(audioMP3)
+    # get path of th
+    # card.pronunciation = audio
+    # showInfo(str(audioMP3.json()))
+    # card.note().__setitem__(cardId, "TEMP")
+    if cardSide == "Front":
+      card.note().__setitem__("Front", card.note().items()[0][1] + "[sound:" + audioLink +"]")
+      showInfo(card.note().items()[0][1])
+      mw.col.update_note(card.note())
+    else:
+      card.note().__setitem__("Back", card.note().items()[0][1] + "[sound:" + 96 +"]")
+      showInfo(card.note().items()[1][1])
+      mw.col.update_note(card.note())
+    # showInfo(card.note().items()[0][1])
+  else:
+    showInfo(f"No standard pronunciation exists for {word}")
 
     # if the pronouciation exists, download it and add it to the card. Delete the downloaded pronouciation after it is applied to the card.
     # if the pronouciation DOES NOT exist, then use showinfo(word) stating that we ran into an error fetching this pronouciation
